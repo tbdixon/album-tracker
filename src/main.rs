711edb1785a6f31164244a0ae0435860b50386a6
Blake Dixon <tbdixon@gmail.com>
2021-01-08 15:20:51 -0500

    tweaking discogs logic

--- src/main.rs
+++ src/main.rs
@@ -97,8 +97,7 @@ async fn discog_query(
     client: &reqwest::Client,
     album_query: &str,
 ) -> Result<String, Box<dyn Error>> {
-    print!("{} -> ", album_query);
-    io::stdout().flush()?;
+    println!("{} -> ", album_query);
     let user_name = env::var("AT_DISCOGS_USER")?;
     let token = env::var("AT_DISCOGS_TOKEN")?;
     let resp = client
@@ -112,7 +111,42 @@ async fn discog_query(
         .await?
         .text()
         .await?;
-    Ok(serde_json::from_str::<Value>(&resp)?["results"][1]["id"].to_string())
+    // the trait `serde_json::value::Index` is not implemented for `RangeTo<{integer}>`
+    let master_id = &serde_json::from_str::<Value>(&resp)?["results"][0]["master_id"];
+    let resp = client
+        .get(&format!(
+            "https://api.discogs.com/masters/{}/versions?format=Vinyl&sort=released&sort_order=desc&country=US&token={}",
+            master_id, token
+        ))
+        .header("Content-Type", "application/json; charset=utf-8")
+        .header("User-Agent", format!("AlbumTracker/{}", user_name))
+        .send()
+        .await?
+        .text()
+        .await?;
+    let choice_1 = &serde_json::from_str::<Value>(&resp)?["versions"][0];
+    let choice_2 = &serde_json::from_str::<Value>(&resp)?["versions"][1];
+    let choice_3 = &serde_json::from_str::<Value>(&resp)?["versions"][2];
+    let choice_4 = &serde_json::from_str::<Value>(&resp)?["versions"][3];
+    let choice_5 = &serde_json::from_str::<Value>(&resp)?["versions"][4];
+    let choice_6 = &serde_json::from_str::<Value>(&resp)?["versions"][5];
+    let choice_7 = &serde_json::from_str::<Value>(&resp)?["versions"][6];
+    let choice_8 = &serde_json::from_str::<Value>(&resp)?["versions"][7];
+    let choice_9 = &serde_json::from_str::<Value>(&resp)?["versions"][8];
+    let choice_10 = &serde_json::from_str::<Value>(&resp)?["versions"][9];
+    let choices = vec![
+        choice_1, choice_2, choice_3, choice_4, choice_5, choice_6, choice_7, choice_8, choice_9,
+        choice_10,
+    ];
+    for (idx, choice) in choices.iter().enumerate() {
+        println!(
+            "{}: {} {} {} {}",
+            idx, choice["title"], choice["country"], choice["released"], choice["format"]
+        );
+    }
+    let mut resp = String::new();
+    std::io::stdin().read_line(&mut resp)?;
+    Ok(choices[resp.trim().parse::<usize>().unwrap()]["id"].to_string())
 }
 
 async fn discog_update(
@@ -152,11 +186,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
     for image_path in image_files {
         print!(">>");
         io::stdout().flush()?;
-        let encoded_image = encode_image(image_path.to_str().unwrap())?;
+        let path_str = &image_path.to_str().unwrap();
+        if &path_str[path_str.len() - 9..] == "PROCESSED" {
+            println!("Skipping processed image <<");
+            continue;
+        }
+        let encoded_image = encode_image(path_str)?;
         let album_info = block_on(google_image(&client, &encoded_image))?;
         let discog_id = block_on(discog_query(&client, &album_info))?;
         block_on(discog_update(&client, &discog_id))?;
-        println!("<<:");
+        fs::rename(&image_path, format!("{}_PROCESSED", path_str))?;
+        println!("<<");
     }
     Ok(())
 }
